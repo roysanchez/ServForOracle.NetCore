90717e98d758609d80528c9bc5a2a5016a7322db
Roy Sanchez <roysanchez@outlook.com>
2018-11-27 11:15:47 -0400

    Added param creation shorthands

--- ServForOracle.NetCore/Parameters/Param.cs
+++ ServForOracle.NetCore/Parameters/Param.cs
@@ -17,5 +17,22 @@ namespace ServForOracle.NetCore.Parameters
         public virtual Type Type { get; }
         public virtual object Value { get; protected set; }
         internal abstract void SetOutputValue(object value);
+
+        public static Param Create<T>(T value, ParameterDirection direction)
+        {
+            var type = typeof(T);
+            if(type.IsValueType || type == typeof(string))
+            {
+                return new ParamCLRType<T>(value, direction);
+            }
+            else
+            {
+                return new ParamObject<T>(value, direction);
+            }
+        }
+
+        public static Param Input<T>(T value) => Create(value, ParameterDirection.Input);
+        public static Param Output<T>() => Create(default(T), ParameterDirection.Output);
+        public static Param InputOutput<T>(T value) => Create(value, ParameterDirection.InputOutput);
     }
 }
