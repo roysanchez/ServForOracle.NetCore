3d250636bf98aa647eaf4cad07149d7a66466164
Roy Sanchez <roysanchez@outlook.com>
2018-11-29 14:37:47 -0400

    Updated the realease to 0.6-beta and fixes the deploy configuration

--- nuget-deploy.sh
+++ nuget-deploy.sh
@@ -1,2 +1,2 @@
-dotnet pack ServForOracle.NetCore --include-source --include-symbols --no-build -o $TRAVIS_BUILD_DIR
-nuget push ServForOracle.NetCore.nupkg -source https://api.nuget.org/v3/index.json
+dotnet pack ServForOracle.NetCore --include-source -o $TRAVIS_BUILD_DIR
+dotnet nuget push ServForOracle.NetCore.*.nupkg --source https://api.nuget.org/v3/index.json --api-key $NUGET_TOKEN
