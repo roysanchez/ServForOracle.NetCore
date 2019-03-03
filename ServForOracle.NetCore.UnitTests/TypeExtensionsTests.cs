cedbd4b1d3d7059c57a7853a0032e4009bbea757
Roy Sanchez <roysanchez@outlook.com>
2019-03-03 02:12:22 -0400

    Added the unit test for the TypeExtensions class.

---
+++ ServForOracle.NetCore.UnitTests/TypeExtensionsTests.cs
@@ -0,0 +1,106 @@
+﻿using ServForOracle.NetCore.Extensions;
+using ServForOracle.NetCore.UnitTests.Config;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+using System.Text;
+using Xunit;
+
+namespace ServForOracle.NetCore.UnitTests
+{
+    public class TypeExtensionsTests
+    {
+        class TestClass
+        {
+            public TestClass(string test)
+            {
+
+            }
+        }
+
+        [Fact]
+        public void IsBoolean_ReturnsTrue()
+        {
+            Assert.True(TypeExtensions.IsBoolean(typeof(bool)));
+        }
+
+        [Fact]
+        public void IsBoolean_ReturnsFalse()
+        {
+            Assert.False(TypeExtensions.IsBoolean(typeof(string)));
+        }
+
+        [Fact]
+        public void IsClrType_ReturnsTrue()
+        {
+            Assert.True(TypeExtensions.IsClrType(typeof(string)));
+            Assert.True(TypeExtensions.IsClrType(typeof(int)));
+        }
+
+        [Fact]
+        public void IsClrType_ReturnsFalse()
+        {
+            Assert.False(TypeExtensions.IsClrType(typeof(object)));
+        }
+
+        [Fact]
+        public void IsCollection_ReturnsTrue()
+        {
+            Assert.True(TypeExtensions.IsCollection(typeof(object[])));
+            Assert.True(TypeExtensions.IsCollection(typeof(IEnumerable<object>)));
+            Assert.True(TypeExtensions.IsCollection(typeof(List<object>)));
+            Assert.True(TypeExtensions.IsCollection(typeof(Collection<object>)));
+            Assert.True(TypeExtensions.IsCollection(typeof(IList<object>)));
+        }
+
+        [Fact]
+        public void IsCollection_ReturnsFalse()
+        {
+            Assert.False(TypeExtensions.IsCollection(typeof(object)));
+            Assert.False(TypeExtensions.IsCollection(typeof(IEqualityComparer<object>)));
+        }
+
+        [Fact]
+        public void GetCollectionUnderType_ReturnsGenericType()
+        {
+            var type = typeof(object);
+            Assert.Equal(type, TypeExtensions.GetCollectionUnderType(typeof(object[])));
+            Assert.Equal(type, TypeExtensions.GetCollectionUnderType(typeof(IEnumerable<object>)));
+            Assert.Equal(type, TypeExtensions.GetCollectionUnderType(typeof(List<object>)));
+            Assert.Equal(type, TypeExtensions.GetCollectionUnderType(typeof(Collection<object>)));
+            Assert.Equal(type, TypeExtensions.GetCollectionUnderType(typeof(IList<object>)));
+        }
+
+        [Fact]
+        public void GetCollectionUnderType_NotCollection_ReturnsNull()
+        {
+            Assert.Null(TypeExtensions.GetCollectionUnderType(typeof(int)));
+            Assert.Null(TypeExtensions.GetCollectionUnderType(typeof(string)));
+        }
+
+        [Fact]
+        public void GetCollectionUnderType_Invalid_ReturnsNull()
+        {
+            Assert.Null(TypeExtensions.GetCollectionUnderType(typeof(int)));
+            Assert.Null(TypeExtensions.GetCollectionUnderType(typeof(string)));
+        }
+
+        [Theory, CustomAutoData]
+        public void CreateInstance_Works(string ctorParameter)
+        {
+            Assert.IsType<List<object>>(TypeExtensions.CreateInstance(typeof(List<object>)));
+            Assert.IsType<object[]>(TypeExtensions.CreateInstance(typeof(object[])));
+            Assert.IsType<TestClass>(TypeExtensions.CreateInstance(typeof(TestClass), ctorParameter));
+            Assert.IsType<int>(TypeExtensions.CreateInstance(typeof(int)));
+        }
+
+        [Fact]
+        public void CreateListType_Works()
+        {
+            Assert.Equal(typeof(List<object>),TypeExtensions.CreateListType(typeof(object)));
+            Assert.Equal(typeof(List<List<object>>),TypeExtensions.CreateListType(typeof(List<object>)));
+            Assert.Equal(typeof(List<object[]>),TypeExtensions.CreateListType(typeof(object[])));
+        }
+    }
+}
