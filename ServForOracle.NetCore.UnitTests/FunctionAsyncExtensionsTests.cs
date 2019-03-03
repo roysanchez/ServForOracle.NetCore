8cdc260bec027d9ce436610e0aa7785a1f8a6284
Roy Sanchez <roysanchez@outlook.com>
2019-03-02 21:39:31 -0400

    Fixed issues with the FunctionAsyncExtensions and added the unit tests.

---
+++ ServForOracle.NetCore.UnitTests/FunctionAsyncExtensionsTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoFixture;
+using Moq;
+using ServForOracle.NetCore.Parameters;
+using ServForOracle.NetCore.UnitTests.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ServForOracle.NetCore.UnitTests
+{
+    public class FunctionAsyncExtensionsTests
+    {
+        [Theory, CustomAutoData]
+        public async Task ExecuteFunctionAsync_Works_OneParameter<T, A>(Mock<IServiceForOracle> serviceMock, string function)
+        {
+            var fixture = new Fixture();
+            var value = fixture.Create<A>();
+            var expectedValue = fixture.Create<T>();
+
+            serviceMock.Setup(s => s.ExecuteFunctionAsync<T>(function, It.IsAny<IParam>()))
+                .ReturnsAsync(expectedValue);
+
+            var actual = await FunctionAsyncExtensions.ExecuteFunctionAsync<T,A>(serviceMock.Object, function, value);
+
+            Assert.Equal(expectedValue, actual);
+        }
+
+        [Theory, CustomAutoData]
+        public async Task ExecuteFunctionAsync_Works_TwoParameters<T, A, B>(Mock<IServiceForOracle> serviceMock, string function)
+        {
+            var fixture = new Fixture();
+            var valueA = fixture.Create<A>();
+            var valueB = fixture.Create<B>();
+            var expectedValue = fixture.Create<T>();
+
+            serviceMock.Setup(s => s.ExecuteFunctionAsync<T>(function, It.IsAny<IParam>(), It.IsAny<IParam>()))
+                .ReturnsAsync(expectedValue);
+
+            var actual = await FunctionAsyncExtensions.ExecuteFunctionAsync<T, A, B>(serviceMock.Object, function, valueA, valueB);
+
+            Assert.Equal(expectedValue, actual);
+        }
+
+        [Theory, CustomAutoData]
+        public async Task ExecuteFunctionAsync_Works_ThreeParameters<T, A, B, C>(Mock<IServiceForOracle> serviceMock, string function)
+        {
+            var fixture = new Fixture();
+            var valueA = fixture.Create<A>();
+            var valueB = fixture.Create<B>();
+            var valueC = fixture.Create<C>();
+            var expectedValue = fixture.Create<T>();
+
+            serviceMock.Setup(s => s.ExecuteFunctionAsync<T>(function, It.IsAny<IParam>(), It.IsAny<IParam>(), It.IsAny<IParam>()))
+                .ReturnsAsync(expectedValue);
+
+            var actual = await FunctionAsyncExtensions.ExecuteFunctionAsync<T, A, B, C>(serviceMock.Object, function, valueA, valueB, valueC);
+
+            Assert.Equal(expectedValue, actual);
+        }
+        [Theory, CustomAutoData]
+        public async Task ExecuteFunctionAsync_Works_FourParameters<T, A, B, C, D>(Mock<IServiceForOracle> serviceMock, string function)
+        {
+            var fixture = new Fixture();
+            var valueA = fixture.Create<A>();
+            var valueB = fixture.Create<B>();
+            var valueC = fixture.Create<C>();
+            var valueD = fixture.Create<D>();
+
+            var expectedValue = fixture.Create<T>();
+
+            serviceMock.Setup(s => s.ExecuteFunctionAsync<T>(function, It.IsAny<IParam>(), It.IsAny<IParam>(), It.IsAny<IParam>(), It.IsAny<IParam>()))
+                .ReturnsAsync(expectedValue);
+
+            var actual = await FunctionAsyncExtensions.ExecuteFunctionAsync<T, A, B, C, D>(serviceMock.Object, function, valueA, valueB, valueC, valueD);
+
+            Assert.Equal(expectedValue, actual);
+        }
+    }
+}
--- ServForOracle.NetCore/ExecuteExtensions/FunctionAsyncExtensions.cs
+++ ServForOracle.NetCore/ExecuteExtensions/FunctionAsyncExtensions.cs
@@ -10,11 +10,11 @@ namespace ServForOracle.NetCore
     {
         public static async Task<T> ExecuteFunctionAsync<T, A>(this IServiceForOracle s, string function, A p1)
     => await s.ExecuteFunctionAsync<T>(function, Param.Input(p1));
-        public static async Task ExecuteFunctionAsync<T,A,B>(this IServiceForOracle s, string function, A p1, B p2)
+        public static async Task<T> ExecuteFunctionAsync<T,A,B>(this IServiceForOracle s, string function, A p1, B p2)
             => await s.ExecuteFunctionAsync<T>(function, Param.Input(p1), Param.Input(p2));
-        public static async Task ExecuteFunctionAsync<T,A,B,C>(this IServiceForOracle s, string function, A p1, B p2, C p3)
+        public static async Task<T> ExecuteFunctionAsync<T,A,B,C>(this IServiceForOracle s, string function, A p1, B p2, C p3)
             => await s.ExecuteFunctionAsync<T>(function, Param.Input(p1), Param.Input(p2), Param.Input(p3));
-        public static async Task ExecuteFunctionAsync<T,A,B,C,D>(this IServiceForOracle s, string function, A p1, B p2, C p3, D p4)
+        public static async Task<T> ExecuteFunctionAsync<T,A,B,C,D>(this IServiceForOracle s, string function, A p1, B p2, C p3, D p4)
             => await s.ExecuteFunctionAsync<T>(function, Param.Input(p1), Param.Input(p2), Param.Input(p3), Param.Input(p4));
     }
 }
