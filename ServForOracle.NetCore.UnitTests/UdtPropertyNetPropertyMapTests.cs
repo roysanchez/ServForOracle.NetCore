24389714b6ef302abb53350c1e7c701373be3692
Roy Sanchez <roysanchez@outlook.com>
2019-03-08 12:02:48 -0400

    Added the tests for the UdtPropertyNetPropertyMap class

---
+++ ServForOracle.NetCore.UnitTests/UdtPropertyNetPropertyMapTests.cs
@@ -0,0 +1,36 @@
+﻿using ServForOracle.NetCore.Metadata;
+using ServForOracle.NetCore.UnitTests.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ServForOracle.NetCore.UnitTests
+{
+    public class UdtPropertyNetPropertyMapTests
+    {
+        [Fact]
+        public void Constructor_NullParameters_ThrowsArgumentNull()
+        {
+            Assert.Throws<ArgumentNullException>("netPropertyName", () => new UdtPropertyNetPropertyMap(null, "test"));
+            Assert.Throws<ArgumentNullException>("udtPropertyName", () => new UdtPropertyNetPropertyMap("test", null));
+        }
+
+        [Theory, CustomAutoData]
+        public void Constructor_PropertiesSavedInUpperCase(string netProperty, string udtProperty)
+        {
+            var map = new UdtPropertyNetPropertyMap(netProperty, udtProperty);
+
+            Assert.Equal(netProperty.ToUpper(), map.NetPropertyName);
+            Assert.Equal(udtProperty.ToUpper(), map.UDTPropertyName);
+        }
+
+        [Theory, CustomAutoData]
+        internal void IEquitable_Works(UdtPropertyNetPropertyMap x, UdtPropertyNetPropertyMap y)
+        {
+            Assert.True(x.Equals(x));
+            Assert.False(x.Equals(y));
+            Assert.False(x.Equals(null));
+        }
+    }
+}
