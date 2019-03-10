6af9249f2f98174f401dab674f98ffbe0fa29a3a
Roy Sanchez <roysanchez@outlook.com>
2019-03-10 01:18:13 -0400

    Added the test for the OracleUdtPropertyAttribute class

---
+++ ServForOracle.NetCore.UnitTests/OracleUdtPropertyAttributeTests.cs
@@ -0,0 +1,17 @@
+﻿using ServForOracle.NetCore.OracleAbstracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ServForOracle.NetCore.UnitTests
+{
+    public class OracleUdtPropertyAttributeTests
+    {
+        [Fact]
+        public void Constructor_ThrowsArgumentNull()
+        {
+            Assert.Throws<ArgumentNullException>("propertyName", () => new OracleUdtPropertyAttribute(null));
+        }
+    }
+}
